94c1508981705edfa4c1258a4aee3a160c6b713a
yevhenii <krutsiak>
2026-05-20 18:06:38 +0300

    Add jsBasics homework

---
+++ jsBasics/task1.js
@@ -0,0 +1,11 @@
+const name = 'Yevhenii';
+let age = 29;
+let isUkrainian = true;
+let phoneNumber;
+let email = null;
+
+console.log(name);
+console.log(age);
+console.log(isUkrainian);
+console.log(phoneNumber);
+console.log(email);
---
+++ jsBasics/task3.js
@@ -0,0 +1,5 @@
+let firstPersonAge = 29;
+let isAdult = firstPersonAge >= 18;
+
+console.log(firstPersonAge);
+console.log(isAdult);
