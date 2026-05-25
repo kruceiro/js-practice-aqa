83d97243c965c2ebc7871338d0a25102618edcfc
yevhenii <krutsiak>
2026-05-25 14:21:58 +0300

    conditions and loops homework

---
+++ conditions and loops/task1.js
@@ -0,0 +1,17 @@
+// Task 1
+
+const averageGrade = 100;
+
+if (averageGrade < 0 || averageGrade > 100) {
+    console.log('Invalid value');
+} else if (averageGrade < 60) {
+    console.log('Unsatisfactory');
+} else if (averageGrade >= 60 && averageGrade <= 70) {
+    console.log('Satisfactory');
+} else if (averageGrade >= 71 && averageGrade <= 80) {
+    console.log('Good');
+} else if (averageGrade >= 81 && averageGrade <= 90) {
+    console.log('Very good');
+} else {
+    console.log('Excellent');
+}
