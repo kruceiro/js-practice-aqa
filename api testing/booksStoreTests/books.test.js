92959253cfda59273453b24d69720d8ae1e11ddc
yevhenii <krutsiak>
2026-07-23 20:36:29 +0300

    Add API controllers

--- api testing/booksStoreTests/books.test.js
+++ api testing/booksStoreTests/books.test.js
@@ -1,29 +1,33 @@
-const axios = require('axios');
+const bookStoreController = require('./controllers/BookStoreController');
+const accountController = require('./controllers/AccountController');
+
 describe('Auth', () => {
     test('Login to the system', async () => {
-        const response = await axios.post('https://demoqa.com/Account/v1/login',
-            {
-                "userName": 'krc',
-                "password": 'QqWwEe1!'
-            });
-        console.log(response.data);
+        const response = await accountController.login(
+            'krc',
+            'QqWwEe1!'
+        );
+
         expect(response.status).toBe(200);
         expect(response.data.token).not.toBeNull();
         expect(response.data.username).toBe('krc');
-
     });
 });
+
 describe('Books list', () => {
     test('Get books', async () => {
-        const response = await axios.get('https://demoqa.com/BookStore/v1/Books');
+        const response = await bookStoreController.getBooks();
+
         expect(response.status).toBe(200);
-        expect(response.data.books).toHaveLength(8);
+        expect(response.data.books.length).toBeGreaterThan(0);
     });
 
     test('Get Learning JavaScript Design Patterns book', async () => {
-        const response = await axios.get('https://demoqa.com/BookStore/v1/Book?ISBN=9781449331818');
+        const isbn = '9781449331818';
+        const response = await bookStoreController.getBookByIsbn(isbn);
+
         expect(response.status).toBe(200);
-        expect(response.data.isbn).toBe('9781449331818');
+        expect(response.data.isbn).toBe(isbn);
         expect(response.data.title).toBe('Learning JavaScript Design Patterns');
 
     });
@@ -31,49 +35,33 @@ describe('Books list', () => {
 
 describe('Add books', () =>
     test('Add book', async () => {
-        const responseAuth = await axios.post('https://demoqa.com/Account/v1/login',
-            {
-                'userName': 'krc',
-                'password': 'QqWwEe1!'
-            });
-
-        const responseBook = await axios.post('https://demoqa.com/BookStore/v1/Books',
-            {
-                'userId': responseAuth.data.userId,
-                'collectionOfIsbns': [{
-                    'isbn': '9781449331818'
-                }
-                ]
-            }, {
-            headers: {
-                'Authorization': `Bearer ${responseAuth.data.token}`
-            }
-        }
+        const isbn = '9781449331818';
+        const responseAuth = await accountController.login(
+            'krc',
+            'QqWwEe1!'
         );
-        console.log(responseBook.data);
+
+        const responseBook = await bookStoreController.addBook(
+            responseAuth.data.userId,
+            isbn,
+            responseAuth.data.token
+        );
         expect(responseBook.status).toBe(201);
-        expect(responseBook.data.books[0].isbn).toBe('9781449331818');
+        expect(responseBook.data.books[0].isbn).toBe(isbn);
     }));
 
 describe('Delete books', () => {
     test('Delete book', async () => {
-        const responseAuth = await axios.post('https://demoqa.com/Account/v1/login',
-            {
-                userName: 'krc',
-                password: 'QqWwEe1!'
-            }
+        const isbn = '9781449331818';
+        const responseAuth = await accountController.login(
+            'krc',
+            'QqWwEe1!'
         );
 
-        const responseBook = await axios.delete('https://demoqa.com/BookStore/v1/Book',
-            {
-                headers: {
-                    Authorization: `Bearer ${responseAuth.data.token}`
-                },
-                data: {
-                    userId: responseAuth.data.userId,
-                    isbn: '9781449331818'
-                }
-            }
+        const responseBook = await bookStoreController.deleteBook(
+            responseAuth.data.userId,
+            isbn,
+            responseAuth.data.token
         );
         expect(responseBook.status).toBe(204);
     });
